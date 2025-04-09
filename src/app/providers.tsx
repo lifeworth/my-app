10cb7581a43c22a8d291eedeca61e844e08d2dc4
杜致远 <duzhiyuan@1.com>
2025-04-09 21:35:38 +0800

    初始化

---
+++ src/app/providers.tsx
@@ -0,0 +1,22 @@
+"use client";
+import * as React from "react";
+import { HeroUIProvider } from "@heroui/system";
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps,
+} from "next-themes";
+import { useRouter } from "next/navigation";
+
+export interface ProvidersProps {
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
+}
+
+export function Providers({ children, themeProps }: ProvidersProps) {
+  const router = useRouter();
+  return (
+    <HeroUIProvider navigate={router.push}>
+      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+    </HeroUIProvider>
+  );
+}
